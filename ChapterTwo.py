1886048d1481b2ce40933e2eca5dbe7f43127a6d
Claudiu04 <148909030+Claudiu04@users.noreply.github.com>
2025-10-29 14:00:17 +0200

    Finished Chapter Two

--- ChapterTwo.py
+++ ChapterTwo.py
@@ -18,7 +18,7 @@ def talk_and_move():
         posture.goToPosture("StandInit", 0.5)
 
         player.post.playFile("/home/nao/ChapterTwoMp3/AmTraitAstfel.mp3")
-        
+
         names = [ "LWristYaw", "LElbowYaw", "LElbowRoll", "LHand","LShoulderRoll", 
                   "RWristYaw", "RElbowYaw", "RElbowRoll", "RHand","RShoulderRoll",
                   "HeadPitch"]
@@ -65,11 +65,11 @@ def talk_and_move():
 
         motion.setAngles(["HeadYaw", "HeadPitch"], [0.9 , -0.1 ], 0.2)
 
-        time.sleep(4)
+        time.sleep(7)
 
         player.post.playFile("/home/nao/ChapterTwoMp3/Poftim.mp3")
 
-        time.sleep(4)
+        time.sleep(7)
 
         posture.goToPosture("StandInit", 0.5)
 
@@ -100,6 +100,180 @@ def talk_and_move():
         time.sleep(2)
 
         posture.goToPosture("StandInit", 0.5)
+
+        time.sleep(1)
+
+        names = [ "LWristYaw", "LElbowYaw", "LElbowRoll", "LHand", 
+                  "RWristYaw", "RElbowYaw", "RElbowRoll", "RHand"]
+
+        for i in range(5):
+            angles = [-1.0, -1.0, -1.4, 1.0,
+                      1.0, 1.0,  1.4, 1.0]  
+            motion.setAngles(names, angles, 0.2)
+            time.sleep(2)
+
+            angles = [-0.50, -2.0, -0.04, 1.0,
+                      0.50, 2.0, 0.04, 1.0]
+            motion.setAngles(names, angles, 0.2)
+            time.sleep(2)
+
+            angles = [-0.2, -1.4, -0.8, 1.0,
+                       0.2, 1.4,  0.8, 1.0] 
+            motion.setAngles(names, angles, 0.2)
+            time.sleep(2)
+
+        time.sleep(12)    
+
+
+        player.post.playFile("/home/nao/ChapterTwoMp3/DarCeFaci.mp3")
+
+        names = ["LShoulderRoll", "LElbowRoll", "LElbowYaw" ,"LWristYaw", "LHand", 
+                 "RShoulderRoll", "RElbowRoll", "RElbowYaw" ,"RWristYaw", "RHand",
+                 "HeadPitch", "HeadYaw"]
+
+
+        angles = [0.35, -0.04, -2.0, -0.50, 1.0,
+                  -0.35, 0.04, 2.0, 0.50, 1.0,
+                  0.0, 0.9]  
+
+        motion.setAngles(names, angles, 0.2)
+
+        time.sleep(4)
+
+        posture.goToPosture("StandInit", 0.5)
+
+        time.sleep(8)
+
+        player.post.playFile("/home/nao/ChapterTwoMp3/CandMisterul.mp3")
+
+        names = [ "LWristYaw", "LElbowYaw", "LElbowRoll", "LHand","LShoulderRoll", 
+                  "RWristYaw", "RElbowYaw", "RElbowRoll", "RHand","RShoulderRoll",
+                  "HeadPitch"]
+        
+        for i in range(2):
+            angles = [-0.9, -1.3, -1.0, 0.8,  0.2,
+                       0.9,  1.3,  1.0, 0.8, -0.2,
+                       -0.1]  
+            motion.setAngles(names, angles, 0.2)
+            time.sleep(2)
+
+            angles = [-1.1, -1.0, -1.2, 1.0,  0.4,
+                       1.1,  1.0,  1.2, 1.0, -0.4,
+                      -0.05]
+            motion.setAngles(names, angles, 0.2)
+            time.sleep(2)
+
+            angles = [-1.0, -1.2, -0.9, 0.7,  0.3,
+                       1.3,  1.5,  1.0, 0.9, -0.6,
+                       -0.15] 
+            motion.setAngles(names, angles, 0.2)
+            time.sleep(2)
+
+            angles = [-1.2, -1.4, -0.7, 0.8,  0.1,
+                       1.2,  1.4,  0.7, 0.8, -0.1,
+                      -0.05]
+            motion.setAngles(names, angles, 0.2)
+            time.sleep(2)
+
+            angles = [-0.8, -1.0, -1.1, 0.6,  0.2,
+                       0.8,  1.0,  1.1, 0.6, -0.2,
+                       0.1]
+            motion.setAngles(names, angles, 0.2)
+            time.sleep(2)
+            angles = [-1.2, -1.9, -0.6, 1.0,  0.6,
+                       1.2,  1.9,  0.6, 1.0, -0.6,
+                       0.1] 
+            motion.setAngles(names, angles, 0.2)
+            time.sleep(2)
+
+        names = ["HeadPitch", "HeadYaw"]
+        angles = [0.0, 0.9]  
+
+        motion.setAngles(names, angles, 0.2)
+
+        time.sleep(5)
+            
+
+        posture.goToPosture("StandInit", 0.5)
+
+        time.sleep(4)
+
+        player.post.playFile("/home/nao/ChapterTwoMp3/CumNuDesenasem.mp3")
+
+        time.sleep(8)
+
+        names = ["HeadPitch", "HeadYaw"]
+        angles = [0.0, 0.9]  
+
+        motion.setAngles(names, angles, 0.2)
+
+        time.sleep(15)
+
+        posture.goToPosture("StandInit", 0.5)
+
+        time.sleep(7)
+
+        player.post.playFile("/home/nao/ChapterTwoMp3/AtunciAmCreeat.mp3")
+
+        names = ["LShoulderPitch","LShoulderRoll", "LElbowRoll", "LHand"]
+        angles = [-1.0, 0.8, -0.06, 1.0]  
+        motion.setAngles(names, angles, 0.5)
+
+        time.sleep(2)
+
+        posture.goToPosture("StandInit", 0.5)
+
+        time.sleep(8)
+
+        player.post.playFile("/home/nao/ChapterTwoMp3/AmDesenatDinnou.mp3")
+
+        time.sleep(1)
+
+        names = ["HeadPitch", "HeadYaw"]
+        angles = [0.0, 0.9]  
+
+        motion.setAngles(names, angles, 0.2)
+
+        time.sleep(1)
+
+        posture.goToPosture("StandInit", 0.5)
+
+        time.sleep(10)
+
+        player.playFile("/home/nao/ChapterTwoMp3/AmRefacut.mp3")
+
+        time.sleep(12)
+
+        player.post.playFile("/home/nao/ChapterTwoMp3/AtunciFaraSa.mp3")
+
+        time.sleep(5)
+
+        names = ["RShoulderPitch","RShoulderRoll", "RElbowRoll", "RHand"]
+        angles = [-1.2, -0.8, 0.1, 1.0]  
+
+        motion.setAngles(names, angles, 0.3)
+
+        time.sleep(2)
+
+        posture.goToPosture("StandInit", 0.5)
+
+        time.sleep(14)
+
+        player.playFile("/home/nao/ChapterTwoMp3/DeCe.mp3")
+
+        time.sleep(7)
+
+        player.playFile("/home/nao/ChapterTwoMp3/VaFiDeAjuns.mp3")
+
+        time.sleep(6)
+
+        player.playFile("/home/nao/ChapterTwoMp3/SiAstfel.mp3")
+
+        time.sleep(3)
+
+        posture.goToPosture("StandInit", 0.5)
+
+
 
 
     except Exception as e:
